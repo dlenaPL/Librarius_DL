d6bcaeeb4d2cefce74c22a0b111f6741ea399196
dlenaPL <dlenarciak01@gmail.com>
2024-12-05 22:24:20 +0100

    Collapsable menu v1

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -23,6 +23,47 @@ namespace Librarius_DL
         public MainWindow()
         {
             InitializeComponent();
+            hideSubMenus();
+        }
+
+
+
+        private void toggleVisibility(StackPanel panel)
+        {
+            if (panel.Visibility == Visibility.Collapsed)
+                panel.Visibility = Visibility.Visible;
+            else if (panel.Visibility == Visibility.Visible)
+                panel.Visibility = Visibility.Collapsed;
+        }
+
+        private void hideSubMenus()
+        {
+            edit_submenu.Visibility = Visibility.Collapsed;
+            settings_submenu.Visibility = Visibility.Collapsed;
+        }
+
+        private void clearRadioBtnsIsChecked(StackPanel sp)
+        {
+            foreach (RadioButton r in sp.Children)
+            {
+                if (r.IsChecked == true) r.IsChecked = false;
+            }
+        }
+
+        private void btn_edit_Click(object sender, RoutedEventArgs e)
+        {
+            toggleVisibility(edit_submenu);
+            btn_edit.IsChecked = false;
+            clearRadioBtnsIsChecked(settings_submenu);
+
+        }
+
+
+        private void btn_settings_Click(object sender, RoutedEventArgs e)
+        {
+            toggleVisibility(settings_submenu);
+            btn_settings.IsChecked = false;
+            clearRadioBtnsIsChecked(edit_submenu);
         }
     }
 }
--- ViewModels/HomeVM.cs
+++ ViewModels/HomeVM.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using Librarius_DL.Utilities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace Librarius_DL.ViewModels
 {
